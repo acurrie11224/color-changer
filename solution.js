d0c4f95eb60300e32e08cbd24a05f8fa29abfc93
acurrie11224 <acurr7243@withs.org>
2016-04-14 14:33:54 +0000

    color-changer

--- solution.js
+++ solution.js
@@ -1,1 +1,25 @@
+var colors = ["pink", "purple", "blue", "yellow"];
+
+ function changeColor (){
+    var randomIndex= Math.floor((Math.random() * colors.length) + 1) - 1;
+    $("body").css("background", colors[randomIndex]);
+}
+
+function addColor(){
+    var newColors= $("#newColor").val();
+    colors.push(newColors);
+    $("body").css("background", colors[colors.length -1]);
+   
+} 
+
+$(document).ready(function(){
+    $("#change").click(changeColor);
+    $("#add").click(addColor);
+});
+
+
+
+
+
+
 
